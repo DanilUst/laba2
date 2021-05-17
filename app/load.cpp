ef813878bd76b4058a3615ba855557d28451f810
DanilUst <78728660+DanilUst@users.noreply.github.com>
2021-05-17 12:41:06 +0300

    Update load.cpp

--- app/load.cpp
+++ app/load.cpp
@@ -13,7 +13,7 @@ void load(text txt, string filename)
     char buf[MAXLINE + 1];
 
     char str[100];
-    strcpy(str, filename.c_str());
+    strncpy(str, filename.c_str());
 
 
     if ((f = fopen(str, "r")) == NULL) {

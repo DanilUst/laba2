7a74a492519ee6e549f1005b688a4e791e0bc45f
DanilUst <78728660+DanilUst@users.noreply.github.com>
2021-05-17 20:11:14 +0300

    Update load.cpp

--- app/load.cpp
+++ app/load.cpp
@@ -1,37 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 #include "common.h"
+#include <iostream>
 #include "text.h"
 using namespace std;
 /**
  * Загружает содержимое указанного файла
  */
-void load(text txt, string filename)
+void load(text txt, std::string filename)
 {
-     FILE *f;
+    std::ifstream f;
+    f.open(filename);
 
-    char buf[MAXLINE + 1];
-
-    char str[100];
-    strcpy(str, filename.c_str());
-
-
-    if ((f = fopen(str, "r")) == NULL) {
-        printf("The file cannot be opened\n");
+    if (!f.good()) {
+        std::cerr << "The file " << filename << " cannot be opened\n";
         return;
     }
 
+    
     remove_all(txt);
 
-    while (fgets(buf, MAXLINE, f)) {
+    std::string buf;
 
+    while (f.good()) {
+        f.getline(&buf[0], MAXLINE);
         append_line(txt, buf);
     }
 
-
-    fclose(f);
+    f.close();
 }
-
     
 
 
